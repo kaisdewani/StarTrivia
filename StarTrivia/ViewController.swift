b8399e7f1ad4fbca997d21278e69a6052d257439
Kais Dewani <kaisdewani@yahoo.com>
2019-06-06 18:49:40 -0400

    Initial Commit

---
+++ StarTrivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarTrivia
+//
+//  Created by Kais Dewani on 6/6/19.
+//  Copyright © 2019 Kais Dewani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
