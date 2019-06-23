ada44c51a7bbdc0451969097010133d98b80b25a
Kais Dewani <kaisdewani@yahoo.com>
2019-06-23 18:58:54 -0400

    added Films view controller, model, and api. Films UI has been created as well.

--- StarTrivia/Model/Film.swift
+++ StarTrivia/Model/Film.swift
@@ -5,26 +5,25 @@
 //  Created by Kais Dewani on 6/23/19.
 //  Copyright © 2019 Kais Dewani. All rights reserved.
 //
-
 import Foundation
 
 struct Film: Codable {
     
     let title: String
-    let episode: String
+    let episode: Int
     let crawl: String
     let director: String
     let producer: String
     let releaseData: String
     
-    enum CodingKeys: String, CodingKeys {
-        
+    
+    enum CodingKeys: String, CodingKey {
         case title
         case episode = "episode_id"
         case director
         case crawl = "opening_crawl"
         case producer
         case releaseData = "release_date"
-        
     }
+    
 }
--- StarTrivia/Services/FilmApi.swift
+++ StarTrivia/Services/FilmApi.swift
@@ -16,7 +16,19 @@ class FilmsApi {
         guard let url = URL(string: url) else { return }
         Alamofire.request(url).responseJSON { (response) in
             if let error = response.result.error {
-                
+                debugPrint(error.localizedDescription)
+                completion(nil)
+                return
+            }
+            
+            guard let data = response.data else { return  completion(nil) }
+            let jsonDecoder = JSONDecoder()
+            do {
+                let film = try jsonDecoder.decode(Film.self, from: data)
+                completion(film)
+            } catch {
+                debugPrint(error.localizedDescription)
+                completion(nil)
             }
         }
     }
